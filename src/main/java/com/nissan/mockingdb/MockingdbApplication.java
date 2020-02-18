a6aca5216ffbe8760196307f2e000fff4ab3e516
RAHMANI, MOHD IZHAR <mohdizhar.rahmani@nissanmotor.com>
2020-02-18 13:47:57 +0530

    Project updated with J2EE Arc

---
+++ src/main/java/com/nissan/mockingdb/MockingdbApplication.java
@@ -0,0 +1,13 @@
+package com.nissan.mockingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockingdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockingdbApplication.class, args);
+	}
+
+}
